203266a4e338d3a023f67aff6e0e5ef26551ed24
Cheiley <chelaashley2@gmail.com>
2022-09-09 10:25:50 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - print the string in the put function
  *
- * Description: using the main function
+ * Desciption: using the main function
  * this program prints 'with proper grammar, but the outcome is a piece of art,
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {printf(“with proper grammar, but the outcome is a piece of art, \n”);
-		return (0);
+	return (0);
 }
