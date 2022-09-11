c1911d5ad8f11d569ee68c6c0318de7d6f3320af
Cheiley <chelaashley2@gmail.com>
2022-09-11 13:09:36 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints if the number is positive, negative or zero
+ * Description: using the main function
+ * this program prints 'Programming is positive, negative or zero
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+	putchar(n);
+}
+putchar('\n');
+return (0);
+}
