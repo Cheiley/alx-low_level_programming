4ac9211bd3e0c3ff46ea80c6000d50acc26ef955
Cheiley <chelaashley2@gmail.com>
2022-09-11 07:41:13 -0700

    Positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this will assign a number to the variable
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+		if (n == 0)
+		{
+			printf(% i is zero\n, n);
+		}
+		else
+			if (n < 0)
+			{
+				printf(% i is negative\n, n)
+			}
+			else
+			{
+			       printf(% i is positive\n, n)
+			}       
+					return (0);
+}
