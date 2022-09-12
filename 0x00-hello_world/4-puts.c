6f383d2eaac7d28478c61c56562da0cadf2de68d
Cheiley <chelaashley2@gmail.com>
2022-09-12 15:32:19 -0700

    pUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - prints the program
  * Description: using the main function
  * this program prints 'programming is like building a mutilingual puzzle
  * Return: 0
@@ -9,7 +9,7 @@
 int main(void)
 {
 	puts("\"Programming is like building a mutilingual puzzle");
-	return (0);
+return (0);
 }
 
 
