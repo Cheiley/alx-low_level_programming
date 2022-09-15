13063583746bc630f67cdf134fc70ab152b25c27
Cheiley <chelaashley2@gmail.com>
2022-09-15 09:17:05 -0700

    Mains

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * largest_number - this function returns the largest of three numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
